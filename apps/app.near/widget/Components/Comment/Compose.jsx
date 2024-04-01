eafcb9cb9a012a9f6bd1e30c7e11200422c17eb4
Muhammad Zeeshan Ahmad <itexpert120@outlook.com>
2024-04-01 20:52:29 +0500

    Remove root item

--- apps/app.near/widget/Components/Comment/Compose.jsx
+++ apps/app.near/widget/Components/Comment/Compose.jsx
@@ -3,7 +3,6 @@ if (!context.accountId) {
 }
 
 const item = props.item;
-const rootItem = props.rootItem;
 
 if (!context.accountId) {
   return "";
@@ -12,7 +11,7 @@ if (!context.accountId) {
 const composeData = () => {
   const data = {
     post: {
-      comment: JSON.stringify(Object.assign({ item, rootItem }, state.content)),
+      comment: JSON.stringify(Object.assign({ item }, state.content)),
     },
     index: {
       comment: JSON.stringify({
@@ -31,7 +30,7 @@ const composeData = () => {
 
   const notifications = state.extractMentionNotifications(
     state.content.text,
-    thisItem,
+    thisItem
   );
 
   if (props.notifyAccountId && props.notifyAccountId !== context.accountId) {
@@ -46,7 +45,7 @@ const composeData = () => {
 
   if (notifications.length) {
     data.index.notify = JSON.stringify(
-      notifications.length > 1 ? notifications : notifications[0],
+      notifications.length > 1 ? notifications : notifications[0]
     );
   }
 
@@ -57,7 +56,7 @@ const composeData = () => {
       hashtags.map((hashtag) => ({
         key: hashtag,
         value: thisItem,
-      })),
+      }))
     );
   }
 
